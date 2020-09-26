f779da88208c2dd4df2457cc008c076a8a7e95ae
BarnabasMasil <71864854+BarnabasMasil@users.noreply.github.com>
2020-09-26 13:52:03 -0500

    Update User.java

--- User.java
+++ User.java
@@ -3,7 +3,7 @@ public class User{
   private String loginUsername;
   private String loginPassword;
   
-  private HashTableMap<String, Data<String, String>> credentials;
+  private HashTableMap<String, Data> credentials;
   
   public User(String username, String password){
     loginUsername = username;
@@ -12,6 +12,26 @@ public class User{
   }
   
   public boolean addCredential(String url, String username, String password){
-    return credentials.put(url, new Data<>(url,username,password));
+    return credentials.put(url, new Data(url,username,password));
   }
+  
+  public void printCredentials(String url) {
+    System.out.println(credentials.get(url).getUrl());
+    System.out.println(credentials.get(url).getUsername());
+    System.out.println(credentials.get(url).getPassword());
+    
+  }
+  
+  public String getLoginUsername() {
+    return loginUsername;
+  }
+  
+  public String getLoginPassword() {
+    return loginPassword;
+  }
+  
+  public HashTableMap<String, Data> getCredentials() {
+    return credentials;
+  }
+  
 }
