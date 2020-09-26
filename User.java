9340365b229f6f664f3082ef660b794c844a5d88
BarnabasMasil <71864854+BarnabasMasil@users.noreply.github.com>
2020-09-26 00:36:58 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,17 @@
 public class User{
   
+  private String loginUsername;
+  private String loginPassword;
+  
+  private HashTableMap data;
+  
+  public User(String username, String password){
+    loginUsername = username;
+    loginPassword = password;
+    data = new HashTableMap();
+  }
+  
+  public boolean addCredential(){
+    
+  }
 }
