6ba0f6de4deb373a7ac0b5f128d3b5a706d64c37
BarnabasMasil <71864854+BarnabasMasil@users.noreply.github.com>
2020-09-26 00:24:41 -0500

    Add files via upload

---
+++ Element.java
@@ -0,0 +1,36 @@
+// --== CS400 File Header Information ==--
+// Name: Barnabas Masil Adrian anak Christopher
+// Email: adriananakch@wisc.edu
+// Team: GA
+// TA: Daniel Kiel
+// Lecturer: Gary Dahl
+// Notes to Grader:
+
+/**
+ * This is a generic class that takes 2 generic values which are the KeyType and the ValueType.
+ * Instances of this class represent the key-value pairings
+ * 
+ * @author barnabas
+ *
+ * @param <KeyType>
+ * @param <ValueType>
+ */
+public class Element<KeyType, ValueType> {
+
+  private ValueType value;
+  private KeyType key;
+
+  public Element(ValueType value, KeyType key) {
+    this.value = value;
+    this.key = key;
+  }
+
+  public ValueType getValue() {
+    return value;
+  }
+
+  public KeyType getKey() {
+    return key;
+  }
+
+}
